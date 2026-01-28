ff00ebc0fa3fcba3b9f6eadfb48b33e52c7f916f
JCatalano <casmir72@live.com>
2026-01-28 16:56:54 -0500

    classnotes

---
+++ demo.r
@@ -0,0 +1,16 @@
+numstudents <- 2
+journal <- "CRDH"
+month <- "Sept"
+year <- "1902"
+paste (journal, month, year, ".docx", sep="_")
+
+student <- c("Emma", "Dori", "Sharon", "Josh")
+student [1:3]
+
+
+nrow (BostonWomenVoters)
+
+number_of_rows <- nrow (BostonWomenVoters)
+
+number_of_rows > 100
+number_of_rows = 100
